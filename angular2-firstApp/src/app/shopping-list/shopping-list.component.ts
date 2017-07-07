fe6d7ebcce9a4351b1da22aa6f2fc3b8114ce216
Sam <crisamdegracia@gmail.com>
2017-07-07 22:36:12 +0300

    learned models and nested components

---
+++ angular2-firstApp/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient(
+      'apples',
+      99
+    ),
+    new Ingredient (
+      'orange',
+      32
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
